5985028d18da3cf87ee7f06978a810a669d6c903
David Jonsson <davidjons@gmail.com>
2013-04-09 18:22:19 +0200

    'pg'

--- web.rb
+++ web.rb
@@ -2,8 +2,6 @@ require 'sinatra'
 require 'gcm'
 require 'data_mapper'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'])
-
 class User
 	include DataMapper::Resource
 	property :id, Serial
@@ -12,7 +10,14 @@ class User
 	property :update_at, DateTime
 end
 
-DataMapper.finalize.auto_upgrade!
+configure do
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+	DataMapper.finalize
+	DataMapper.auto_migrate!
+
+	User.create(:name => "David")
+end
+
 
 # List all users
 get '/' do
